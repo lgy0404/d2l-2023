1e79ab8a48c33b1bd0a3e6a0ba7deafc1b5afa69
lgy0404 <2528379691@qq.com>
2023-09-02 16:10:26 +0800

    更新了019卷积层的笔记

--- california-house-prices/houseprice.py
+++ california-house-prices/houseprice.py
@@ -8,18 +8,18 @@ train_data = TabularDataset('train.csv')
 # print(train_data)
 
 # 记录Id列和Sold Price列
-id, lable = 'Id','Sold Price'
+id, label = 'Id', 'Sold Price'  # 修复拼写错误
 
 # 数据预处理
-large_val_cols = ['Lot', 'Total interior livable area','Tax assessed value', 'Annual tax amount','Listed Price','Last Sold Price']
-for c in large_val_cols + [lable]:
+large_val_cols = ['Lot', 'Total interior livable area', 'Tax assessed value', 'Annual tax amount', 'Listed Price', 'Last Sold Price']
+for c in large_val_cols + [label]:
     train_data[c] = np.log(train_data[c] + 1)
-    
+
 # 训练
-predictor = TabularPredictor(label = lable).fit(train_data.drop(columns = [id])) 
+predictor = TabularPredictor(label=label).fit(train_data.drop(columns=[id]))
 
 # 预测
 test_data = TabularDataset('test.csv')
-preds = predictor.leaderboard(test_data.drop(columns = [id]))
-submission = pd.Dataframe({id:test_data[Id], lable:preds})
-submission.to_csv('submission.csv', index = False)
+preds = predictor.predict(test_data.drop(columns=[id]))  # 使用predict函数进行预测
+submission = pd.DataFrame({id: test_data[id], label: preds})  # 修复Dataframe拼写错误
+submission.to_csv('submission.csv', index=False)
